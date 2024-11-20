4726e6230ea2b84fb238426c4dd728ac63b168cc
paceto <proofofrandomness@gmail.com>
2024-11-20 08:13:05 +0200

    WIP

--- src/Frontend/contentRender.js
+++ src/Frontend/contentRender.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 const Header = ({ setRenderSettings }) => {
     useEffect(() => {
         setRenderSettings({
+            disableCopyButton: true,
             disableCodeExecuteButton: true,
             inputLabelsQuickSend: true,
         });
